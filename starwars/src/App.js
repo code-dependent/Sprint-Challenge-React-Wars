ae102f744b0d5c611063ab30bca0d76c72ed76c5
joshua williams <joshua@joshuas-MBP.fios-router.home>
2020-04-17 15:24:57 -0400

    finished

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,49 +1,56 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-
 import { Button } from 'reactstrap';
 import Characters from './components/Character';
 import axios from 'axios';
 import Container from './components/Container';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+
   const [character, setCharacter]=useState([])
   const [nextPageUrl, setNextPageUrl] = useState('')
   const [prevPageUrl, setPrevPageUrl] = useState('')
-
   const [page, setPage] = useState('https://rickandmortyapi.com/api/character/')
-
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
   
 useEffect(()=>{
-  axios.get(page)
-  .then(res=>{
-    setCharacter(res.data.results)
-    setNextPageUrl(res.data.info.next)
-    setPrevPageUrl(res.data.info.prev)
 
-    console.log(res.data, "here")
+  axios.get(page)
+  
+  .then(res=>{
+
+    setCharacter(res.data.results)
+
+    setNextPageUrl(res.data.info.next)
+
+    setPrevPageUrl(res.data.info.prev)
   })
+
   .catch(err=>{
+
     debugger})
+
 },[page])
+
+
 const nextPageHandler = ()=>{
+
   if(nextPageUrl != ''){
+
     return setPage(nextPageUrl)
   }
 }
-const prevPageHandler = ()=>{
-  if(prevPageUrl != '')
-  setPage(prevPageUrl)
 
+const prevPageHandler = ()=>{
+
+  if(prevPageUrl != ''){
+
+  setPage(prevPageUrl)
+  }
 }
   return (
+
     <Container className="App">
+
       <Button style={{width: '40px', height:'30px'}} onClick={prevPageHandler}>Prev. Page</Button>
 
       <Button style={{width: '40px', height:'30px'}} onClick={nextPageHandler}>Next Page</Button>
